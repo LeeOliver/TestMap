b0348a0ba162ebbd178643de07d6311fe639c468
liu122468551 <122468551@qq.com>
2014-04-02 16:23:45 +0800

    Initial Commit

---
+++ TestMap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestMap
+//
+//  Created by yuan on 14-4-2.
+//  Copyright (c) 2014年 lian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
